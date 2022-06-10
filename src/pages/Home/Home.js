5b2e521afb12d2e1b9742a9ab7fb5139f9457913
DianaCPardo <dcpardoa@correo.udistrital.edu.co>
2022-06-10 04:30:44 -0500

    .

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "./Home.css";
 import Header from "../../componentes/Header/Header";
-
+import Footer from "../../componentes/Footer";
 function Home(){
     return(
         <div>       
         <Header/>
-        <h1>Home</h1>
+
+        <body className="body">
+            <h1>Para Revisar mis páginas, da click en el botón "Tu Cuenta" </h1>  
+
+            <h7>Tuve alguns problemas para imprimir algunos textos</h7>  
+        </body>  
+        
+        <Footer/>
         </div>
     );
 }
